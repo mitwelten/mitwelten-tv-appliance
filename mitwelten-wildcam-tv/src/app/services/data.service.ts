4e650bd0ad784e309f6efab2ec3d9295cd982854
Cedric Spindler <cedric.spindler@gmail.com>
2024-06-19 13:39:20 +0200

    Use apiUrl from environment

--- mitwelten-wildcam-tv/src/app/services/data.service.ts
+++ mitwelten-wildcam-tv/src/app/services/data.service.ts
@@ -2,21 +2,20 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { StackQuery } from '../shared/stack-query.type';
 import { StackImage } from '../shared/stack-image.type';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  private apiUrl = 'http://localhost:8000';
-
   constructor(private readonly http: HttpClient) { }
 
   public getImageStack(selection: StackQuery) {
-    return this.http.post<StackImage[]>(`${this.apiUrl}/tv/stack-selection/`, selection);
+    return this.http.post<StackImage[]>(`${environment.apiUrl}/tv/stack-selection/`, selection);
   }
 
   public getImageResource(url: string) {
-    return this.http.get(`${this.apiUrl}/tv/file/${url}`, {responseType: 'blob'});
+    return this.http.get(`${environment.apiUrl}/tv/file/${url}`, {responseType: 'blob'});
   }
 }
---
+++ mitwelten-wildcam-tv/src/environments/environment.ts
@@ -0,0 +1,3 @@
+export const environment = {
+  apiUrl: 'https://data.mitwelten.org/api/v3',
+};
